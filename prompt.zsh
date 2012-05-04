95c7680da0e9b6b770c901060c9941a2cdf6c7f2
drc <double-oh-seven@web.de>
2012-05-04 10:20:50 +0200

    Cleaned prompt.zsh

--- prompt.zsh
+++ prompt.zsh
@@ -6,17 +6,6 @@ else
 	CCOLOR="green";
 fi
 
-# this one works, too, but ~ uses shortcuts, which looks ugly
-#function currentdir {
-#	local directory=${(%):-%~}
-#	local count=$(echo $directory | tr -cd "/" | wc -c)
-#	if [ $count -gt 3 ]; then
-#		echo "…/"%3~
-#	else
-#		echo %~
-#	fi
-#}
-
 function currentdir {
 	local directory=${PWD/$HOME/"~"}
 	local count=$(echo $directory | tr -cd "/" | wc -c)
@@ -28,9 +17,3 @@ function currentdir {
 }
 
 PROMPT='%{$fg[$NCOLOR]%}%m%{$reset_color%}:%{$fg[green]%}$(currentdir)%{$reset_color%}:%{$fg[$CCOLOR]%}%(!.#.$)%{$reset_color%} '
-RPROMPT='%{$fg[green]%}$(git_prompt_info)%{$reset_color%}%'
-
-ZSH_THEME_GIT_PROMPT_PREFIX="<%{$fg[red]%}"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[green]%} %{$fg[yellow]%}✗%{$fg[green]%}>%{$reset_color%}"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[green]%}>"
