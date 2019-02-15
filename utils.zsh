abfcfabdbe60008a27becc3b3f51d93acd5d9d63
Philipp Specht <philipp.specht@ucware.de>
2019-02-15 16:48:18 +0100

    Add percol utilities

--- utils.zsh
+++ utils.zsh
@@ -13,3 +13,22 @@ randline() {
     integer z=$(wc -l <$1)
     sed -n $[RANDOM % z + 1]p $1
 }
+
+ppgrep() {
+    if [[ $1 == "" ]]; then
+        PERCOL=percol
+    else
+        PERCOL="percol --query $1"
+    fi
+    ps aux | eval $PERCOL | awk '{ print $2 }'
+}
+
+ppkill() {
+    if [[ $1 =~ "^-" ]]; then
+        QUERY=""            # options only
+    else
+        QUERY=$1            # with a query
+        [[ $# > 0 ]] && shift
+    fi
+    ppgrep $QUERY | xargs kill $*
+}
